3072c3860e62fa0b16cb24cbc14b58c42f39b1d3
cwensel <cwensel@72861afd-6043-0410-8b40-956228ca1d42>
2008-07-07 20:18:10 +0000

    compiles and tests well against hadoop 0.17

---
+++ branches/hadoop_0.17/cascading/src/java/cascading/tuple/TupleCollector.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) 2007-2008 Concurrent, Inc. All Rights Reserved.
+ *
+ * Project and contact information: http://www.cascading.org/
+ *
+ * This file is part of the Cascading project.
+ *
+ * Cascading is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Cascading is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Cascading.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package cascading.tuple;
+
+import java.io.IOException;
+
+import cascading.operation.Operation;
+
+/** Interface TupleCollector is used to allow {@link Operation} instances to emit result {@link Tuple} values. */
+public abstract class TupleCollector
+  {
+  /** Field declared */
+  protected Fields declared;
+
+  protected TupleCollector()
+    {
+    }
+
+  /**
+   * Constructor TupleCollector creates a new TupleCollector instance.
+   *
+   * @param declared of type Fields
+   */
+  public TupleCollector( Fields declared )
+    {
+    if( declared == null )
+      throw new IllegalArgumentException( "declared fields must not be null" );
+
+    this.declared = declared;
+    }
+
+  /**
+   * Method add inserts the given {@link TupleEntry} into the outgoing stream.
+   *
+   * @param entry of type TupleEntry
+   */
+  public void add( TupleEntry entry )
+    {
+    add( entry.getTuple() );
+    }
+
+  /**
+   * Method add inserts the given {@link Tuple} into the outgoing stream.
+   *
+   * @param tuple of type Tuple
+   */
+  public void add( Tuple tuple )
+    {
+    if( tuple.isEmpty() )
+      return;
+
+    if( declared != null && !declared.isUnknown() && declared.size() != tuple.size() )
+      throw new TupleException( "operation added the wrong number of fields, expected: " + declared.print() + ", got result size: " + tuple.size() );
+
+    collect( tuple );
+    }
+
+  protected abstract void collect( Tuple tuple );
+
+  /**
+   * Method close closes the underlying resource being written to. This method should be called when no more {@link Tuple}
+   * instances will be written out.
+   *
+   * @throws IOException when
+   */
+  public void close() throws IOException
+    {
+    // do nothing
+    }
+
+  }
